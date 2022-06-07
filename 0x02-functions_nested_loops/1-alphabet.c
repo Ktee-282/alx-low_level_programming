494b552ec65a0753818470891ea8634372d76a02
Ktee-282 <koladeomotayo01@gmail.com>
2022-06-07 03:52:03 -0700

    Write a function that prints 10 times the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet, in lowercase, followed by a new line.
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
  *
  * Return: Always 0 (success)
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	char ch = 'a';
 
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase, followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int n = 1;
+	char ch = 'a';
+
+	for (; n <= 10;)
+	{
+		for(; ch <= 'z';)
+		{
+			_putchar(ch);
+			ch++;
+		}
+		n++;
+	}
+
+	_putchar('\n');
+	return (0);
+}
