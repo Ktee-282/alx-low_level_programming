b82b4dc2c3be33d4b3bd2792e7b35748a6cecbd4
Ktee-282 <koladeomotayo01@gmail.com>
2022-06-14 13:01:55 -0700

    prints the lowercase alphabet in reverse, followed by a new line

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/***
+/**
  * main - prints the lowercase alphabet in reverse, followed by a new line
  * Return: Always 0 (success)
  */
@@ -14,5 +14,5 @@ for (ch = 'z'; ch >= 'a'; ch--)
 putchar(ch);
 }
 putchar('\n');
-return(0);
+return (0);
 }
