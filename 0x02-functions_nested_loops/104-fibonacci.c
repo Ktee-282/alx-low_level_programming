50042168f3e11f796a44df2ff226f5317b9639c6
Ktee-282 <koladeomotayo01@gmail.com>
2022-09-19 03:35:32 -0700

    finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,13 +13,13 @@ long int n1, n2, fn, fn2, n11, n22;
 n1 = 1;
 n2 =2;
 boolean = boolean2 = 1;
-printf("%1d, %1d", n1, n2);
+printf("%ld, %ld", n1, n2);
 for (c = 0; c < 96; c++)
 {
 if (boolean)
 {
 fn = n1 + n2;
-printf(", %1d", fn);
+printf(", %ld", fn);
 n1 = n2;
 n2 = fn;
 }
@@ -35,8 +35,8 @@ boolean2 = 0;
 }
 fn2 = (n11 + n22);
 fn = n1 + n2 + (fn2 / 1000000000);
-printf(", %1d", fn);
-printf("%1d", fn2 % 1000000000);
+printf(", %ld", fn);
+printf("%ld", fn2 % 1000000000);
 n1 = n2;
 n11 = n22;
 n2 = fn;
