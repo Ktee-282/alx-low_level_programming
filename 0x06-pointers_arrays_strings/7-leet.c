9abe21d8b2679b2eb7707e445e6f499b5febfe6a
Ktee-282 <koladeomotayo01@gmail.com>
2022-09-23 04:29:59 -0700

    encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *lest - encodes a string into 1337
+ * *lest - encodes a string into * @s: pointer to char
  * @s: pointer to char
  *
  * Return: *s
@@ -9,18 +9,18 @@
 
 char *lest(char *s)
 {
-int i, j;
-char l[] = "ol_ea__t";
+	int i, j;
+	char l[] = "ol_ea__t";
 
-for (i = 0; s[i] != '\0'; i++)
-{
-for (j = 0; l[j] != '\0'; j++)
-{
-if (s[i] == l[j] || s[i] == (l[j] - 32))
-{
-s[i] = j + '0';
-}
-}
-}
-return (s);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			if (s[i] == l[j] || s[i] == (l[j] - 32))
+			{
+				s[i] = j + '0';
+			}
+		}
+	}
+	return (s);
 }
