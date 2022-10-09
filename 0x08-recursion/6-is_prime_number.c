a44dab3616acc28a3051c7543baae2824d28972b
Ktee-282 <koladeomotayo01@gmail.com>
2022-10-09 08:45:39 -0700

    returns 1 if the input integer is a prime number, otherwise return 0.

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * is_prime - find a prime number
+ * @i: input int i
+ * @j: input int j
+ * Return: boolean val of the state of inputs
+ */
+
+int is_prime(int i, innt j)
+{
+	if (j == 1)
+	{
+		return (1);
+	}
+	if (i % j == 0)
+	{
+		return (0);
+	}
+	return (is_prime(i, j - 1));
+}
+
+/**
+ * is_prime_number - printing prime numbers
+ * @n: input integer value
+ * Return: the state of the input value
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 3)
+	{
+		return (0);
+	}
+	return (is_prime(n, n - 1));
+}
