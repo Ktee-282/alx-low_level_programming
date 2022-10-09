053b097c50a66ed18de033d8ce9beb032acf62c9
Ktee-282 <koladeomotayo01@gmail.com>
2022-10-09 10:27:39 -0700

    prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives.
+ * @argc: argument count
+ * @argv: string array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		int i;
+
+		for (i = 0; i < argc; i++)
+		{
+			printf("argv[%d] = %s\n", i, argv[i]);
+		}
+	}
+	return (0);
+}
