c604296cdacb62353690e8b03e1fce90b6882824
Ktee-282 <koladeomotayo01@gmail.com>
2022-10-09 11:20:42 -0700

    adds positive numbers.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: first parameter
+ * @argv: second parameter
+ *
+ * Return: 1 if one of the number 
+ * contains symbols that are not digits
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (!(argv[] <= 1000000))
+	{
+		return (printf("Error\n"), 1);
+	}
+	else
+	{
+		for (i = 0; i < argc; i++)
+		{
+			num1 = atoi(argv[1]);
+			num2 = atoi(argv[2]);
+		}
+		sum = num1 + num2;
+		printf("%d\n", sum);
+	}
+	return (0);
+}
