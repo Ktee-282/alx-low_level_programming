a9eed40e738472223f94c32e071f26d49ba8fca8
Ktee-282 <koladeomotayo01@gmail.com>
2022-10-16 13:20:28 -0700

    concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,53 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _strlen(char *str);
 /**
- * str_concat - concatenates two strings
- *
- * @s1:pointer to string one
- * @s2:pointer to string two
- *
- * return: A concatenated string from s1 and s2 and;
- * NULL on failure
+ * str_concat - Concatenate/joins two strings
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
+ * Return: A concatenated string from s1 and s2
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *strjoin;
-	int i, j, x, y, strlen;
+	char *concat;
+	int x, y, a, b;
+
+	x = y = a = b = 0;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	i = 0;
-
-	while (s1[i] != '\0')
-		i++;
-
-	j = 0;
-
-	while (s2[j] != '\0')
-		j++;
-
-	strjoin = malloc(sizeof(char) * (i + j + 1));
-	if (strjoin == NULL)
-		return (NULL);
-
-	x = 0;
 	while (s1[x] != '\0')
 	{
-		strjoin[x] = s1[x];
 		x++;
 	}
-	y = 0;
+
 	while (s2[y] != '\0')
 	{
-		strjoin[x] = s2[y];
-		x++;
 		y++;
 	}
-	strjoin[x] = '\0';
-	return (strjoin);
+
+	concat = malloc(sizeof(char) * (x + y + 1))
+	if (concat == NULL)
+		return (NULL);
+
+	while (s2[a] != '\0')
+	{
+		concat[a] = s1[a];
+		a++;
+	}
+	while (s2[b] != '\0')
+	{
+		concat[a] = s2[b];
+		a++;
+		b++;
+	}
+	concat[a] = '\0';
+	return (concat);
 }
