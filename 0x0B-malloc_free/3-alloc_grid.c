2246bcd614d92e097a43eda2ed42c7b754e12053
Ktee-282 <koladeomotayo01@gmail.com>
2022-10-16 13:38:36 -0700

    returns a pointer to a 2 dimensional array of integers.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	gridA = malloc(sizeof(int) * height);
+	gridA = malloc(sizeof(int *) * height);
 
 	if (gridA == NULL)
 	{
